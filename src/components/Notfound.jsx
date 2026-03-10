f88727b073dfb83623f8b1c154941c3605df3a40
Cedric <stormyz22a@gmail.com>
2026-03-10 14:09:09 +0300

    added react codes

---
+++ src/components/Notfound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/Notfound.css'
+
+const Notfound = () => {
+  return (
+    <div className= 'notfound'>
+      <h1> Ooops!!!Sorry</h1>
+      <h1>Page Not Found</h1>
+      <h1>!Error! 404 !Error!</h1>
+      <a href="/">Back Home</a>
+    </div>
+  )
+}
+
+export default Notfound
