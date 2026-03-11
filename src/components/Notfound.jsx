8f76e20e383b702954040c5b84df481889ce1393
Cedric <stormyz22a@gmail.com>
2026-03-11 14:06:16 +0300

    new react codes

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -6,7 +6,7 @@ const Notfound = () => {
     <div className= 'notfound'>
       <h1> Ooops!!!Sorry</h1>
       <h1>Page Not Found</h1>
-      <h1>!Error! 404 !Error!</h1>
+      <h1>!Error! 404</h1>
       <a href="/">Back Home</a>
     </div>
   )
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { useState } from 'react'
 
 const Signup = () => {
+  // initialize the hooks
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("");
+  const [tel, setTel] = useState("");
+
+
+
   return (
-    <div>
-      <h1>Welcome to the signup page</h1>
+    <div className='row justify-content-center mt-4'>
+      <div className="card col-md-6 shadow p-4">
+        <h1 className='text-primary '>Sign Up</h1>
+
+        <form>
+          <input type="text"
+          placeholder='Enter the Username'
+          className='form-control'
+          value={username}
+          onChange={(e => setUsername(e.target.value))}
+          required/> <br />
+
+          {/* {username} */}
+
+          <input type="email"
+          placeholder='Enter the Email address'
+          className='form-control'
+          value={email}
+          onChange={(e => setEmail(e.target.value))}
+          required /> <br />
+
+          {/* {email} */}
+
+          <input type="password"
+          placeholder='Enter the password'
+          className='form-control'
+          value={password}
+          onChange={(e => setPassword(e.target.value))}
+          required /> <br />
+
+          {/* {password} */}
+
+          <input type="tel"
+          placeholder='Enter your mobile phone number'
+          className='form-control'
+          value={tel}
+          onChange={(e => setTel(e.target.value))}
+          required /> <br />
+
+          {/* {tel} */}
+
+          <input type="button" value="Signup" className='btn-primary'/>
+          <br /> <br />
+
+          Already have an account? <Link to={'/signin'}>Signin</Link>
+        </form>
+      </div>
     </div>
   )
 }
